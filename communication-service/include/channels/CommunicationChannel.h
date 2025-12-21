77e37fffac1bf65d5b22074938078350ccb0956c
Penquilz22 <Huydio2202@gmail.com>
2025-12-21 16:54:00 +0700

    feat(communication-channel:make communication channels inherit ICommunicationService

--- communication-service/include/channels/CommunicationChannel.h
+++ communication-service/include/channels/CommunicationChannel.h
@@ -2,19 +2,24 @@
 #define COMMUNICATION_CHANNEL_H
 
 #include <string>
+#include "../interfaces/ICommunicationService.h"
 
 namespace sdrs::communication
-{ 
+{
 
-class CommunicationChannel
+class CommunicationChannel : public ICommunicationService
 {
 public:
     virtual ~CommunicationChannel() = default;
     virtual std::string getName() const = 0;
     virtual bool send(const std::string& recipient, const std::string& message) = 0;
-
+    bool sendMessage(int accountId, const std::string& message) const override
+    {
+        std::string recipient = std::to_string(accountId);
+        return const_cast<CommunicationChannel*>(this)->send(recipient, message);
+    }
 };
 
 } // namespace sdrs::communication
 
-#endif // COMMUNICATION_CHANNEL_H
+#endif
