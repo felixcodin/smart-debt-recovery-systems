d8f170d47921eac9ed145914d7635d341c87ff5b
felixcodin <brightsmileslikesunshine@gmail.com>
2025-12-27 09:06:11 +0700

    refactor(communication-manager): simplify exception handling by removing namespace prefixes

--- communication-service/src/managers/CommunicationManager.cpp
+++ communication-service/src/managers/CommunicationManager.cpp
@@ -1,5 +1,9 @@
+// CommunicationManager.cpp - Implementation
+
 #include "../../include/managers/CommunicationManager.h"
 #include "../../../common/include/exceptions/ValidationException.h"
+
+using namespace sdrs::exceptions;
 
 namespace sdrs::communication
 {
@@ -8,13 +12,13 @@ void CommunicationManager::registerChannel(std::shared_ptr<CommunicationChannel>
 {
     if (!channel)
     {
-        throw sdrs::exceptions::ValidationException("Communication channel must not be null", "channel");
+        throw ValidationException("Communication channel must not be null", "channel");
     }
 
     const std::string name = channel->getName();
     if (name.empty())
     {
-        throw sdrs::exceptions::ValidationException("Communication channel name must not be empty", "channelName");
+        throw ValidationException("Communication channel name must not be empty", "channelName");
     }
 
     channels_[name] = channel;
@@ -24,14 +28,14 @@ bool CommunicationManager::sendMessage(int accountId,const std::string& message)
 {
     if (message.empty())
     {
-        throw sdrs::exceptions::ValidationException("Message must not be empty","message");
+        throw ValidationException("Message must not be empty","message");
     }
     const std::string recipient = resolveRecipient(accountId);
     const std::string channelName = resolveChannel(accountId);
     auto it = channels_.find(channelName);
     if (it == channels_.end())
     {
-        throw sdrs::exceptions::ValidationException("Communication channel not found", "channelName");
+        throw ValidationException("Communication channel not found", "channelName");
     }
     return it->second->send(recipient, message);
 }
@@ -40,7 +44,7 @@ std::string CommunicationManager::resolveRecipient(int accountId) const
 {
     if (accountId <= 0)
     {
-        throw sdrs::exceptions::ValidationException("Invalid accountId","accountId");
+        throw ValidationException("Invalid accountId","accountId");
     }
     return "user_" + std::to_string(accountId);
 }
